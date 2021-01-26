0a5f70e6a8fda30fafc8b79617823d7dec751c46
Jane <iamjq.82@gmail.com>
2021-01-25 23:12:29 -0500

    Added page on index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 module.exports = router;
